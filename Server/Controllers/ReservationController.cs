7a454309371c58b9970f579c9d61e8432d900900
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-08-17 18:54:01 +0200

    Add reservation logic for different roles

--- Server/Controllers/ReservationController.cs
+++ Server/Controllers/ReservationController.cs
@@ -42,17 +42,75 @@ namespace lib_blazor.Server.Controllers
         [HttpGet]
         public  IEnumerable<ReservationDto> GetAllReservationsAsDtOs()
         {
-            var reservations = _context.Reservations.Include(r => r.User).Include(r => r.Book)
-                .Select(r => new ReservationDto
-                {
-                    Username = r.User!.UserName,
-                    BookTitle = r.Book!.Title,
-                    Author = r.Book.Author,
-                    ReservationId = r.Id
-                })
-                .ToList();
+            var currentUser = HttpContext.User;
+            bool isAdmin = currentUser.HasClaim(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role" && c.Value == "Admin");
+            bool isUser = currentUser.HasClaim(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role" && c.Value == "User");
+            
+            if (isAdmin)
+            {
+                var reservations = _context.Reservations
+                    .Include(r => r.User)
+                    .Include(r => r.Book)
+                    .Select(r => new ReservationDto
+                    {
+                        Username = r.User!.UserName,
+                        BookTitle = r.Book!.Title,
+                        Author = r.Book.Author,
+                        ReservationId = r.Id
+                    })
+                    .ToList();
 
-            return reservations;
+                return reservations;
+            }
+
+            if (isUser)
+            {
+                var userId = currentUser.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                var reservations = _context.Reservations
+                    .Include(r => r.User)
+                    .Include(r => r.Book)
+                    .Where(r => r.User.Id == userId) // Filter by the current user's ID
+                    .Select(r => new ReservationDto
+                    {
+                        Username = r.User!.UserName,
+                        BookTitle = r.Book!.Title,
+                        Author = r.Book.Author,
+                        ReservationId = r.Id
+                    })
+                    .ToList();
+
+                return reservations;
+            }
+            else
+            {
+                return new List<ReservationDto>();
+            }
+        }
+        
+        [HttpDelete("{reservationId}")]
+        public async Task<IActionResult> DeleteReservation(int reservationId)
+        {
+            var reservation = await _context.Reservations
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(r => r.Id == reservationId);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            bool isAdmin = User.HasClaim(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role" && c.Value == "Admin");
+            if (!isAdmin && reservation.User?.Id != userId)
+            {
+                return Forbid();
+            }
+
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return Ok("Reservation deleted successfully!");
         }
     }
 }
