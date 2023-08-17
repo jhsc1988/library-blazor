8fee4ca0d060335a14d435f172df15bb7fbf6700
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-08-17 09:25:28 +0200

    Add Book to db

--- Server/Data/ApplicationDbContext.cs
+++ Server/Data/ApplicationDbContext.cs
@@ -2,12 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Duende.IdentityServer.EntityFramework.Options;
+using lib_blazor.Models;
 using lib_blazor.Server.Models;
 
 namespace lib_blazor.Server.Data;
 
 public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
 {
+    public DbSet<Book> Books { get; set; }
+    
     public ApplicationDbContext(
         DbContextOptions options,
         IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
