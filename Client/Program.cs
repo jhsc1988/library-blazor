a311ff8bd325ab93f880ec79b9c7d5bdf9ce760b
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-08-19 10:19:54 +0200

    Add UserRoleCheck

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using lib_blazor.Client;
+using lib_blazor.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +15,7 @@ builder.Services.AddHttpClient("lib_blazor.ServerAPI", client => client.BaseAddr
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("lib_blazor.ServerAPI"));
 
 builder.Services.AddApiAuthorization();
+
+builder.Services.AddScoped<UserRoleCheck>();
 
 await builder.Build().RunAsync();
